b174fec43c76b16708ab830a02f00cb4244ed892
olaniyiolufemi <o.olufemi@alustudent.com>
2024-06-17 18:01:48 +0200

    olaniyi js-2

--- form.js
+++ form.js
@@ -1,17 +1,22 @@
+const firstnameinput = document.getElementById("firstName");
+const lastnameinput = document.getElementById("lastName");
+const emailinput = document.getElementById("email");
+const passwordinput = document.getElementById("password");
+const subscriptionInputs = document.querySelectorAll('input[name="subscription"]:checked');
+const errormessage = document.getElementsByClassName("error");
+const firstNameRegex = /^[A-Za-z]{3,50}$/;
+
+
 let form = document.getElementById("registrationForm")
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  
-  
-  const firstnameinput = document.getElementById("firstName");
-  const lastnameinput = document.getElementById("lastName");
-  const emailinput = document.getElementById("email");
-  const passwordinput = document.getElementById("password");
-  const subscriptionInputs = document.querySelectorAll('input[name="subscription"]:checked');
-  const errormessage = document.getElementsByClassName("error");
-
-  const firstNameRegex = /^[A-Za-z]{3,50}$/;
-  if (!firstNameRegex.test(firstnameinput.value)) {
-    showErrorMessage
-  }
+ 
+  validation(firstnameinput, firstNameRegex, 0, "input correct name")
 } )
+
+function validation(id, regex, index, message){
+  if (!regex.test(id.value)) {
+    message[index].innerHTML = errormessage
+    
+  } else{errormessage[index].innerHTML = ""}
+}
